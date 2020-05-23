2e496e390e71c91223201d3919ae7c48fac5bc93
Harold <harol.mont97@gmail.com>
2020-05-22 23:06:20 -0500

    Respuesta en JSON y RUTAS para produtos Ready

--- TiendaProducto/Controllers/ProductosController.cs
+++ TiendaProducto/Controllers/ProductosController.cs
@@ -1,6 +1,7 @@
 ﻿using ModelsDb;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,12 +13,62 @@ namespace TiendaProducto.Controllers
     {
         private DbContextMigration dbContext = new DbContextMigration();
 
+        //Visualizar los registro de los productos
         [HttpGet]
         public IEnumerable<Producto> Get()
         {
             using (DbContextMigration productoEntities = new DbContextMigration())
             {
                 return productoEntities.Producto.ToList();
+            }
+        }
+        //Visualizar solo un registro por id(api/producto/id) 
+        [HttpGet]
+        public Producto Get(int id)
+        {
+            using(DbContextMigration productoEntities = new DbContextMigration())
+            {
+                return productoEntities.Producto.FirstOrDefault(e => e.Id == id);
+            }
+        }
+        //Grabar registros de productos nuevo
+        [HttpPost]
+        public IHttpActionResult AgregarProducto([FromBody]Producto pro)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Producto.Add(pro);
+                dbContext.SaveChanges();
+                return Ok(pro);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        //Editar registro
+        [HttpPut]
+        public IHttpActionResult ActualizarProducto(int id, [FromBody]Producto usu)
+        {
+            if (ModelState.IsValid)
+            {
+                var productoExiste = dbContext.Producto.Count(c => c.Id == id) > 0;
+
+                if (productoExiste)
+                {
+                    dbContext.Entry(usu).State = EntityState.Modified;
+                    dbContext.SaveChanges();
+
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest();
             }
         }
     }
